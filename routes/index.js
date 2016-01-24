6dc09d8be5bfc8dc4118de25d02e232631089c9b
z1c0 <wziegler@live.at>
2016-01-24 11:23:57 +0100

    Changed default port

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var path = require('path');
 var Datastore = require('nedb')
 
 
-var db = new Datastore({ filename: path.join(__dirname, '../hugo.datafile'), autoload: true });
+var db = new Datastore({ filename: path.join(__dirname, 'hugo.datafile'), autoload: true });
 
 
 
