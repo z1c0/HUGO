3aef04034f1c6aaf60b54d56be0ab848bbbe1198
z1c0 <wziegler@live.at>
2016-01-26 15:18:46 +0100

    JSON bus data response now easier to consume. Added simple (unstyled) bus monitor view.

--- modules/busmon/busData.js
+++ modules/busmon/busData.js
@@ -2,19 +2,19 @@ var http = require('http');
 var parseString = require('xml2js').parseString;
 
 
-var doRequest = function(callback, urlParams, renderCallback) {
+var doRequest = function (callback, urlParams, renderCallback) {
   var options = {
     host: 'www.linzag.at',
     path: '/static/XML_DM_REQUEST?'
   };
   options.path += urlParams;
   var req = http.request(options);
-  req.on('response', function(res) {
+  req.on('response', function (res) {
     var data = '';
     // concat chunks
-    res.on('data', function(chunk) { data += chunk });  
+    res.on('data', function (chunk) { data += chunk });  
     // when the response has finished
-    res.on('end', function() {          
+    res.on('end', function () {          
       //console log(data);
       parseString(data, function (err, result) {
         // fire callback      
@@ -25,20 +25,19 @@ var doRequest = function(callback, urlParams, renderCallback) {
   req.end();
 }
 
-function filter(line, direction, minutes)
-{
+function filter(line, direction, minutes) {
   return (minutes >= 0 && minutes < 100) &&
-         ((line == "27" && direction == "Linz Fernheizkraftwerk") ||
-          (line == "27" && direction == "Linz Lederergasse") ||
-          (line == "27" && direction == "Linz Hafen") ||
-          (line == "46" && direction == "Linz Hafen") ||
-          (line == "26" && direction == "Linz St. Margarethen"));
+    ((line == "27" && direction == "Linz Fernheizkraftwerk") ||
+      (line == "27" && direction == "Linz Lederergasse") ||
+      (line == "27" && direction == "Linz Hafen") ||
+      (line == "46" && direction == "Linz Hafen") ||
+      (line == "26" && direction == "Linz St. Margarethen"));
 }
 
 function getTimes(result, renderCallback) {
   var departures = result.itdRequest.itdDepartureMonitorRequest[0].itdDepartureList[0].itdDeparture;
   //console.log(departures);
-  var nextDepartures = {}; 
+  var nextDepartures = {};
   for (var i in departures) {
     var d = departures[i];
     var lineNr = d.itdServingLine[0].$.number;
@@ -54,30 +53,35 @@ function getTimes(result, renderCallback) {
       if (direction.indexOf("Linz ") == 0) {
         direction = direction.substr(5);
       }
-      nextDepartures[lineNr] = { to: direction, in: minutes  };
+      nextDepartures[lineNr] = { to: direction, in: minutes };
     }
   }
+  
+  var departureList = [];
+  for (var d in nextDepartures) {
+    departureList.push({ l: d, to: nextDepartures[d].to, in: nextDepartures[d].in }); 
+  }
   //console.log(nextDepartures);
-  renderCallback(nextDepartures);
+  renderCallback(departureList);
 }
 
 function getLines(result, renderCallback) {
-  try { 
+  try {
     var sessionId = result.itdRequest.$.sessionID;
-    var requestId = result.itdRequest.itdDepartureMonitorRequest[0].$.requestID; 
+    var requestId = result.itdRequest.itdDepartureMonitorRequest[0].$.requestID;
     var lines = result.itdRequest.itdDepartureMonitorRequest[0].itdServingLines[0].itdServingLine; 
     //console.log(sessionId);
     //console.log(requestId);
     //console.log(lines.length);
     var urlParams = "sessionID=" + sessionId + "&requestID=" + requestId;
     for (var i in lines) {
-        var line = lines[i].$
-        //console.log("[" + line.index + "] " + line.number + ": " + line.direction);
-        urlParams += ("&dmLineSelection=" + line.index);
-    }  
+      var line = lines[i].$
+      //console.log("[" + line.index + "] " + line.number + ": " + line.direction);
+      urlParams += ("&dmLineSelection=" + line.index);
+    }
     doRequest(getTimes, urlParams, renderCallback);
   } catch (ex) {
-      console.log(ex);      
+    console.log(ex);
   }
 }
 
