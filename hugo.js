a88231400c9fb8b9122456882d2fd8acba94367d
z1c0 <wziegler@live.at>
2016-01-27 20:24:16 +0100

    Added uptime

--- hugo.js
+++ hugo.js
@@ -1,6 +1,11 @@
 var allModules = require('./modules/modules');
+var moment = require('moment');
+var start = moment();
 
 module.exports = {
   title: "H.U.G.O.",
   modules: allModules,
+  uptime: function() {
+    return  moment.duration(moment() - start).humanize();
+  }
 };
