93e4b06348e0f7603ecfc68e2a4318121e91e44e
z1c0 <wziegler@live.at>
2016-10-09 20:49:17 +0200

    Weather is now returning data

--- modules/weather/weatherFetcher.js
+++ modules/weather/weatherFetcher.js
@@ -1,12 +1,34 @@
 "use strict"
+var request = require('request');
+
+let config = {};
 
 function getWeather(callback) {
-  callback({ "foo" : 2 });
+  request.get({
+    url : "http://api.openweathermap.org/data/2.5/weather?q=Linz,AT&appid=" + config.apiKey + "&units=metric&lang=de",
+    json : true 
+  },
+  function(err, httpResponse, body) {
+    if (err) {
+      // TODO
+      console.log(err);
+    }
+    else {
+      //console.log(body);
+      callback({
+        name : body.name,
+        temp : Math.round(body.main.temp),
+        description : body.weather[0].description,
+        image : "http://openweathermap.org/img/w/" + body.weather[0].icon + ".png"
+
+      });
+    }
+  });  
 }
 
 module.exports = {
   init : function(cfg) {
-    //config = cfg;
+    config = cfg;
   },
 
   getWeather : getWeather
